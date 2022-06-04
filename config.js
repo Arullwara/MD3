deb0a25cb98a2a3b3a8ef440b0cdafca1cf24fba
Arullwara <106657360+Arullwara@users.noreply.github.com>
2022-06-04 23:01:27 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-global.owner = ['6285748894171'] // Put your number here
+global.owner = ['6285822347348'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -42,19 +42,19 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.stiker_wait = 'Stiker sedang dibuat'
-global.packname = 'stickerby'
-global.author = `NakataArdnt`
-global.wm = 'NahBotz-' //Nama Bot
+global.packname = 'stickerbyArullBotz'
+global.author = `Arull`
+global.wm = 'ArullBotz-' //Nama Bot
 global.watermark = wm
 global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
-global.instagram = 'https://instagram.com/nakataa1.a'
+global.instagram = 'https://instagram.com/sahrulwara_____'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&doScale=true&scaleWidth=500&scaleHeight=500&fontsize=100&fillTextType=0&backgroundColor=%23401620&text='
-global.wait = '_*tunggu sedang di proses...*_'
+global.wait = '_*sedang diproses sayangg...*_'
 global.eror = '_*Server Error*_'
 global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
 
-global.multiplier = 69 // The higher, The harder levelup
+global.multiplier = 999 // The higher, The harder levelup
 
 global.rpg = {
   emoticon(string) {
